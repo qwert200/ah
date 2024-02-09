32da690b7da88f696862cc2f46b6b2e4730e8795
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-09 17:44:53 +0300

    Update unmute.py

--- AlexaMusic/plugins/admins/unmute.py
+++ AlexaMusic/plugins/admins/unmute.py
@@ -24,7 +24,12 @@ from AlexaMusic.utils.decorators import AdminRightsCheck
 UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
 
 
-@app.on_message(filters.command(UNMUTE_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+ filters.command(UNMUTE_COMMAND,"")
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def unmute_admin(Client, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
