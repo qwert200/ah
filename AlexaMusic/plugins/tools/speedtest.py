1590f3bd42a15b0b3cdbe03959360302365450d9
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-09 20:09:21 +0300

    Update speedtest.py

--- AlexaMusic/plugins/tools/speedtest.py
+++ AlexaMusic/plugins/tools/speedtest.py
@@ -37,7 +37,10 @@ def testspeed(m):
     return result
 
 
-@app.on_message(filters.command(SPEEDTEST_COMMAND) & SUDOERS)
+@app.on_message(
+    command(SPEEDTEST_COMMAND)
+    & SUDOERS
+)
 async def speedtest_function(client, message):
     m = await message.reply_text("💫 ᴛʀʏɪɴɢ ᴛᴏ ᴄʜᴇᴄᴋ ᴜᴩʟᴏᴀᴅ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ sᴩᴇᴇᴅ")
     loop = asyncio.get_event_loop()
